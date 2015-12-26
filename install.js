9f26e548b8a56a190f4c1737f863bce0611ee1b8
amolc <amol.chawathe@fountaintechies.com>
2015-12-26 23:04:12 +0800

    Package Json File

---
+++ install.js
@@ -0,0 +1,10 @@
+/**
+ * @api {get} /user/:id Request User information
+ * @apiName Install
+ * @apiGroup Install
+ *
+ * @apiParam {Number} id Users unique ID.
+ *
+ * @apiSuccess {String} firstname Firstname of the User.
+ * @apiSuccess {String} lastname  Lastname of the User.
+ */
